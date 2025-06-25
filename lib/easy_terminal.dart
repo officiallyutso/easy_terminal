f9fd74f571abb9ad2cf1c3e5277375ca22677f50
officiallyutso <utsosarkar1@gmail.com>
2025-06-25 06:51:33 +0530

    feat: export public api

--- lib/easy_terminal.dart
+++ lib/easy_terminal.dart
@@ -1,5 +1,16 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+library easy_terminal;
+
+
+export 'src/widgets/terminal_view.dart';
+export 'src/widgets/terminal_panel.dart';
+export 'src/widgets/terminal_progress_bar.dart';
+export 'src/widgets/terminal_list_selector.dart';
+export 'src/widgets/terminal_input.dart';
+export 'src/widgets/terminal_text.dart';
+
+export 'src/models/terminal_theme.dart';
+export 'src/models/terminal_command.dart';
+export 'src/models/terminal_buffer.dart';
+
+export 'src/themes/default_themes.dart';
+export 'src/themes/theme_presets.dart';
